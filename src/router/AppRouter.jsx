b930a21a2e0f113b1935ff482ae339c7ba4aea76
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-08-12 00:15:49 +0300

    Updated UpdateProduct component and added route to AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,6 +5,7 @@ import Main from '../pages/Main'
 import ProductList from '../pages/ProductList'
 import NewProduct from '../pages/NewProduct'
 import About from '../pages/About'
+import UpdateProduct from '../pages/UpdateProduct'
 
 const AppRouter = () => {
   return (
@@ -15,7 +16,7 @@ const AppRouter = () => {
             <Route path="/products" element={<ProductList/>}/>
             <Route path="/new-product" element={<NewProduct/>}/>
             <Route path="/about" element={<About/>}/>
-            <Route path="/" element={<Main/>}/>
+            <Route path="/update-product" element={<UpdateProduct/>}/>
         </Routes>
     </Router>
   )
